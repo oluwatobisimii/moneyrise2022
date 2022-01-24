457177de5548a709526529b950d8f459f4abc1cd
oluwatobisimi <bamitobs@gmail.com>
2022-01-24 10:41:23 +0100

    button animations

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -21,7 +21,6 @@ function Hero() {
     containerRef.current.addEventListener(
       "load",
       (e: React.ChangeEvent<HTMLInputElement>) => {
-
         // Hide Initial Image Display
         container3Ref.current.style.display = "none";
 
@@ -42,9 +41,9 @@ function Hero() {
     container2Ref.current.addEventListener(
       "load",
       (e: React.ChangeEvent<HTMLInputElement>) => {
-         // Hide Initial Image Display
-         container3Ref.current.style.display = "none";
-        
+        // Hide Initial Image Display
+        container3Ref.current.style.display = "none";
+
         // 4. configure the interactivity library
         create({
           mode: "scroll",
@@ -61,25 +60,14 @@ function Hero() {
     );
 
     return () => {};
-  }, [container2Ref, containerRef,  container3Ref]);
+  }, [container2Ref, containerRef, container3Ref]);
 
   return (
     <div className="dark:bg-black md:py-6 lg:py-2">
-      <section
-        className="container mx-auto px-5 pt-9 md:pt-12 lg:flex"
-      >
-        <div
-          className="font-dm-sans py-4 lg:w-1/2 lg:self-center">
-          <div
-            className="text-center space-y-6 
-          lg:text-left md:space-y-10 lg:space-y-7
-        "
-          >
-            <p
-              className="font-medium text-18
-          dark:text-darktextHD
-          "
-            >
+      <section className="container mx-auto px-5 pt-9 md:pt-12 lg:flex lg:pt-4">
+        <div className="font-dm-sans py-4 lg:w-1/2 lg:self-center">
+          <div className="text-center space-y-6 lg:text-left md:space-y-10 lg:space-y-7">
+            <p className="font-medium text-18 dark:text-darktextHD">
               The money event of{" "}
               <span className="text-primary2 font-bold">2022 </span>{" "}
               <span>
@@ -97,9 +85,11 @@ function Hero() {
             <p className="font-medium text-18 dark:text-darktextHD">
               Where money meets direction.
             </p>
-            <div className="bg-grey flex space-x-2 text-base items-center shadow-inset justify-center md:space-x-7 p-4 rounded-lg w-fit 
+            <div
+              className="bg-grey flex space-x-2 text-base items-center shadow-inset justify-center md:space-x-7 p-4 rounded-lg w-fit 
             md:px-8 md:py-6
-            dark:bg-bodycopy dark:text-white mx-auto lg:mx-0 lg:w-fit md:text-lg lg:text-base">
+            dark:bg-bodycopy dark:text-white mx-auto lg:mx-0 lg:w-fit md:text-lg lg:text-base"
+            >
               <div className="flex items-center space-x-1 md:space-x-3">
                 <HiCalendar className="text-primary2" />
                 <p>Jan 29</p>
@@ -114,18 +104,23 @@ function Hero() {
               </div>
             </div>
             <a
-            href="https://tix.africa/rise22"
-            target="_blank"
-            rel="noreferrer"
-            className="px-4 py-3 bg-primary2 font-bold text-base text-white inline-block rounded-md hover:bg-btnhover group"
-          >
-            Register Now{" "}
-            <HiArrowRight className="pl-2 inline text-xl font-bold group-hover:animate-bounce" />
-          </a>
+              href="https://tix.africa/rise22"
+              target="_blank"
+              rel="noreferrer"
+              className="px-4 py-3 bg-primary2 font-bold text-base text-white inline-block rounded-md hover:bg-btnhover group"
+            >
+              Register Now{" "}
+              <HiArrowRight className="pl-2 inline text-xl font-bold group-hover:animate-bounce" />
+            </a>
           </div>
         </div>
         <div className="lg:w-1/2">
-          <img src={greencoinshero} alt="" ref={container3Ref} className="dark:hidden hidden"/>
+          <img
+            src={greencoinshero}
+            alt=""
+            ref={container3Ref}
+            className="dark:hidden hidden"
+          />
           <div className="dark:hidden">
             <lottie-player
               ref={containerRef}
